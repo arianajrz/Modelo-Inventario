a28a55c4dbb366642b602ed9fa3e5966763f0dea
Leslie Daniela Romero Sican <leslie.romero0510@gmail.com>
2020-05-18 18:40:08 -0600

    espacio xd

--- Modelo/Class.Conexion.php
+++ Modelo/Class.Conexion.php
@@ -9,11 +9,11 @@
             $user="root";
             $base="proyecto";
             
-            //parent::__construct('mysql.hostinger.com',$user,'geo4160150087',$base);
+             parent::__construct('mysql.hostinger.com',$user,'geo4160150087',$base)  ;
             parent::__construct($server,$user,$pass,$base);
             $this->query("SET NAMES 'utf8';");
             $this->connect_errno ? die('Error en la conexion de la base de datos.') : $error="<br><br> Conectado con la base de datos: ".$base;
-            //echo $error;
+            //echo  $error;
 			unset($error);
        }
      }
